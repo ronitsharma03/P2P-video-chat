b2aa2c41c7852f9d82d560b57d1ccd613b3445c0
Ronit <ronitkhajuria03@gmail.com>
2025-02-21 03:20:55 +0530

    health route added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,12 @@ const server = http.createServer(app);
 
 const signalingServer = SignalingServer.getInstance(server);
 
+app.get("/health", (req, res) => {
+  res.json({
+    message: "Server is healthy"
+  })
+});
+
 server.listen(3000, () => {
   console.log("Server is listening on 3000...");
 });
